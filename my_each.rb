1d26d917b1dda0183ed3ac56f2e85074ae9d8054
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-25 13:01:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,22 +4,10 @@ def my_each(array)
     
     while i < array.length
     yield(array[1])
-    
+    i += 1 
   end
   
   array 
   end
 end
 
-def my_each(array)
- if block_given? 
-   i = 0 
-   
-   while i < array.length 
-   yield(array[i])
-   i += 1 
- end 
- 
- array 
-  end
-end 
